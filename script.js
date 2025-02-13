0c21c1c25bd160f7dcbb537ab93a92f08b0f58b1
unknown <kevinog13@hotmail.com>
2025-02-13 16:28:34 -0600

    1 commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const btnYes = document.getElementById('btnyes');
+const btnNo = document.getElementById('btnno');
+
+
+btnYes.addEventListener('click', () => {
+    window.location.href = './LUTTMATS.htlm';
+});
+
+
+btnNo.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * window.innerWidth);
+    const j = Math.floor(Math.random() * window.innerHeight);
+    btnNo.style.position = 'absolute';
+    btnNo.style.left = i + 'px';
+    btnNo.style.top = j + 'px';
+});
